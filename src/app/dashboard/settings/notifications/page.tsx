c12af6dbc8efcf3939f969fa73e4d6a41553d90a
sivas20 <sivarajannapadali@gmail.com>
2025-08-19 23:05:09 +0530

    feat:added created settings page

--- src/app/dashboard/settings/notifications/page.tsx
+++ src/app/dashboard/settings/notifications/page.tsx
@@ -1,40 +1,56 @@
-import Link from "next/link"
-export default function notifi(){
-    return(
-        <>
-        <div className="min-h-screen bg-gradient-to-br from-blue-50 via-indigo-200 to-purple-400 p-10">
-            <div className="flex flex-col justify-center p-4 max-w-300 mx-auto rounded-xl shadow-xl bg-amber-50">
-                <h1 className="text-4xl p-4 font-bold">Notifications</h1>
-                <div className="p-4">
-                    <h2 className="text-2xl">Notification preferences</h2>
-                    <div className="flex flex-col mt-2">
-                        <span className="text-lg">Email notifications</span>
-                    <select name="" id="" className="rounded-lg border-2 text-lg hover:bg-yellow-100 border-indigo-50 p-2">
-                        <option value="">Enable</option>
-                        <option value="">Disable</option>
-                    </select>
-                    </div>
-                    <div className="flex flex-col mt-2">
-                        <span className="text-lg">SMS notifications</span>
-                    <select name="" id="" className="rounded-lg border-2 text-lg hover:bg-yellow-100 border-indigo-50 p-2">
-                        <option value="">Enable</option>
-                        <option value="">Disable</option>
-                    </select>
-                    </div>
-                    <div className="flex flex-col mt-2">
-                        <span className="text-lg">Push notifications</span>
-                    <select name="" id="" className="rounded-lg border-2 text-lg hover:bg-yellow-100 border-indigo-50 p-2">
-                        <option value="">Enable</option>
-                        <option value="">Disable</option>
-                    </select>
-                    </div>
-                </div>
-                <div className="ml-auto mr-8">
-                    <button className="text-lg mr-2 rounded-2xl bg-amber-80 hover:bg-indigo-100 p-3">Cancel</button>
-                    <button className="text-lg mr-2 rounded-2xl bg-orange-400 text-white hover:bg-yellow-300 hover:text-black p-3">Save Changes</button>
-                </div>
+import Link from "next/link";
+export default function notifi() {
+  return (
+    <>
+      <div className="min-h-screen bg-gradient-to-br from-blue-50 via-indigo-200 to-purple-400 p-10">
+        <div className="flex flex-col justify-center p-4 max-w-300 mx-auto rounded-xl shadow-xl bg-amber-50">
+          <h1 className="text-4xl p-4 font-bold">Notifications</h1>
+          <div className="p-4">
+            <h2 className="text-2xl">Notification preferences</h2>
+            <div className="flex flex-col mt-2">
+              <span className="text-lg">Email notifications</span>
+              <select
+                name=""
+                id=""
+                className="rounded-lg border-2 text-lg hover:bg-yellow-100 border-indigo-50 p-2"
+              >
+                <option value="">Enable</option>
+                <option value="">Disable</option>
+              </select>
             </div>
+            <div className="flex flex-col mt-2">
+              <span className="text-lg">SMS notifications</span>
+              <select
+                name=""
+                id=""
+                className="rounded-lg border-2 text-lg hover:bg-yellow-100 border-indigo-50 p-2"
+              >
+                <option value="">Enable</option>
+                <option value="">Disable</option>
+              </select>
+            </div>
+            <div className="flex flex-col mt-2">
+              <span className="text-lg">Push notifications</span>
+              <select
+                name=""
+                id=""
+                className="rounded-lg border-2 text-lg hover:bg-yellow-100 border-indigo-50 p-2"
+              >
+                <option value="">Enable</option>
+                <option value="">Disable</option>
+              </select>
+            </div>
+          </div>
+          <div className="ml-auto mr-8">
+            <button className="text-lg mr-2 rounded-2xl bg-amber-80 hover:bg-indigo-100 p-3">
+              Cancel
+            </button>
+            <button className="text-lg mr-2 rounded-2xl bg-orange-400 text-white hover:bg-yellow-300 hover:text-black p-3">
+              Save Changes
+            </button>
+          </div>
         </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,9 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-blue-50 to-red-100">
-      <h1 className="text-5xl font-bold">Welcome to Siva online class</h1>
-      <Link href="/dashboard" className="mt-6 border-2 border-gray-400 p-2 rounded-xl bg-blue-400 text-white">Go to Dashboard</Link>
+      <h1 className="text-5xl font-bold">Welcome to The Dashboard App</h1>
+      <p className="text-lg mt-2">Manage your tasks, track analysis and stay organized with our powerful dashboard</p>
+      <Link href="/dashboard" className="mt-6 text-xl border-2 border-gray-400 p-2 rounded-xl bg-blue-400 text-white">Go to Dashboard</Link>
     </div>
   );
 }
